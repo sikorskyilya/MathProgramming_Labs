68faf6cfa81fd347bdda748429e6cafbba00d38d
sikorskyilya <sikorsky.ilya@mail.ru>
2019-04-13 20:31:49 +0300

    l2

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <ctime>
 #include <locale>
-#define  CYCLE 900000 // количество циклов  
+#define  CYCLE 1000000 // количество циклов  
 int main()
 {
 	double  av1 = 0, av2 = 0;
 	clock_t  t1 = 0, t2 = 0;
 	setlocale(LC_ALL, "rus");
+	int k = time(NULL);
 	auxil::start();                          // старт генерации 
 	t1 = clock();                            // фиксация времени 
 	for (int i = 0; i < CYCLE; i++)
@@ -21,7 +22,7 @@ int main()
 	std::cout << std::endl << "среднее значение (int):    " << av1 / CYCLE;
 	std::cout << std::endl << "среднее значение (double): " << av2 / CYCLE;
 	std::cout << std::endl << "продолжительность (мсек):   " << (t2 - t1);
-	std::cout << std::endl << "                  (сек):   " << ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+	std::cout << std::endl << "                  (сек):   " << ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);//кол-во тиков в секунду
 	std::cout << std::endl;
 	system("pause");
 	return 0;
---
+++ Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Auxil.h"
+#include <iostream>
+#include <iomanip> 
+#include <time.h>
+#include "Salesman.h"
+#define SPACE(n) std::setw(n)<<" "
+#define N 11
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int d[N * N + 1], r[N];
+	auxil::start();
+	for (int i = 0; i <= N * N; i++) d[i] = auxil::iget(10, 300);
+	std::cout << std::endl << "-- Задача коммивояжера -- ";
+	std::cout << std::endl << "-- количество ------ продолжительность -- ";
+	std::cout << std::endl << "      городов           вычисления  ";
+	clock_t t1, t2;
+	for (int i = 7; i <= N; i++)
+	{
+		t1 = clock();
+		salesman(i, (int*)d, r);
+		t2 = clock();
+		std::cout << std::endl << SPACE(7) << std::setw(2) << i
+			<< SPACE(15) << std::setw(5) << (t2 - t1);
+	}
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
